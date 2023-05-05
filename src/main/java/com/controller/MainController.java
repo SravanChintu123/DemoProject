065680269a30bdbc90959a7c4fd2c27edf8ec72c
Soumya <myofficedemomail@gmail.com>
2023-05-05 17:47:17 +0530

    Soumya Added

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -31,7 +31,7 @@ public class MainController {
 	public String test() {
 		return "testjsp";
 	}
-	//Commit Again
+	//Commit Again fhjsjhdf
 	@RequestMapping(value="/User",method=RequestMethod.POST)
 	public String UserAdd(@ModelAttribute("user") User user, Model model) throws Exception {
 		System.out.println(user.getSno());
